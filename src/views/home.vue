845267bce4f67f9fee4e6eaeec86d43fa0eb2c4a
Qazi wahaj <qw_wahaj@yahoo.com>
2021-10-14 02:17:18 +0500

    Feat : added vuex store

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>Home page component</div>
+  <div>
+    <span>Home page component </span>
+    <span>
+      SideNavbar value comes from vuex store :
+      {{ $store.getters.isSideNavbar ? "ON" : "OFF" }}
+    </span>
+    <br />
+    <button @click="toggleSideNav">Toggle value in store</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "home",
+  methods: {
+    // calling action toggleNavbar to update value in store which automatically updated in store and all app
+    async toggleSideNav() {
+      await this.$store.dispatch("toggleNavbar");
+    },
+  },
 };
 </script>
 
