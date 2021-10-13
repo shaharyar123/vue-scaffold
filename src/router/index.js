c49ac8c1e18856540514666965e6fb3bfaa9cee6
Qazi wahaj <qw_wahaj@yahoo.com>
2021-10-14 02:40:14 +0500

    Feat : Added authGuard for protected routes.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
+import Store from "../store";
 
 Vue.use(Router);
 
@@ -15,6 +16,28 @@ export default new Router({
       path: "/home",
       name: "home",
       component: () => import("../views/home")
+    },
+    {
+      path: "/protected-route",
+      name: "protected-route",
+      beforeEnter: authGuard,
+      component: () => import("../views/protected")
     }
   ]
 });
+
+function authGuard(to, from, next) {
+  // considering boolean here as a token just for demo purpose
+  const token = Store.getters.isSideNavbar;
+
+  if (token) {
+    // if here means everything seems goods no need to redirect anywhere forcefully
+    next();
+  } else {
+    // if not logged in or token not found, redirect to login or any public route.
+    // in my case redirecting to home.
+    next("/"); // go to '/login';
+  }
+
+  next();
+}
